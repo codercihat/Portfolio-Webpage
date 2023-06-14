26db010a993392dbe107b5084c74629931dd3ea9
codercihat <elwercim6a2@gmail.com>
2023-06-14 14:11:13 +0300

    ooff

--- src/About.tsx
+++ src/About.tsx
@@ -1,5 +1,4 @@
 import { BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
-import { Link } from "react-router-dom";
 
 export default function About(){
     return(
--- src/Project.tsx
+++ src/Project.tsx
@@ -1,5 +1,5 @@
 import { BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
-import { Link } from "react-router-dom";
+
 
 export default function Projects(){
     return( <>
