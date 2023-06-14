373ed5c69f1a2c74cfcb52ca017d1f2c6b6149f1
codercihat <elwercim6a2@gmail.com>
2023-06-14 14:28:30 +0300

    now

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 /* import ErrorPage from "./error-page";
 import Root from "./routes/root";
 import Contact from "./routes/contact"; */
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
-import MainPage from './MainPage';
-import About from './About';
-import Projects from './Project';
-import NewEl from './newEl';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import MainPage from "./MainPage";
+import About from "./About";
+import Projects from "./Project";
+import NewEl from "./newEl";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainPage />,
-    children: [ 
-      {path:"/",
-        element: <NewEl />},
+    children: [
+      { path: "/", element: <NewEl /> },
       {
-      path: "/about",
-      element: <About />,
-    }, 
-    {
-      path: "/projects",
-      element: <Projects />, 
-    }]  
-  
- 
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />,
+      },
+    ],
   },
-   /* {
+  /* {
     path: "/about",
     element: <About />,
   }, 
@@ -41,8 +36,8 @@ const router = createBrowserRouter([
   } */
 ]);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
